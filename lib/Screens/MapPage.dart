991ef67bd4ff1756433f390885705bae681383b3
Pablo Loaiza Mejía <pablo@loaizas.com>
2022-11-20 21:54:05 -0500

    implementación googlemaps

--- lib/Screens/MapPage.dart
+++ lib/Screens/MapPage.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:where_to/src/locations.dart' as locations;
+//import 'package:geolocator/geolocator.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:where_to/Widgets/NavigationMenu.dart';
@@ -11,6 +15,36 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  //late GoogleMapController mapController;
+
+  //void _onMapCreated(GoogleMapController controller) {
+  //  mapController = controller;
+  //}
+
+  //static final CameraPosition _kGooglePlex = CameraPosition(
+  //  target: LatLng(6.240862, -75.590475),
+  //  zoom: 15,
+  //);
+
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -35,18 +69,51 @@ class _MapPageState extends State<MapPage> {
             ),
           ],
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Positioned(
-                  top: 0,
-                  child: Container(
-                    width: size.width,
-                    child: Stack(),
-                  ))
-            ],
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(0, 0),
+            zoom: 2,
           ),
+          markers: _markers.values.toSet(),
         ),
         bottomNavigationBar: NavigationMenu());
   }
 }
+
+/*Future<Position> _getCurrentPosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}*/
