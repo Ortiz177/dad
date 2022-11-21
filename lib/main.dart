de9321a41b76582e479956a5db3ee97e5a4f39a3
Andres Saldarriaga <saldarias753@outlook.com>
2022-11-21 06:46:50 -0500

    add login and register

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:where_to/Screens/LoginPage.dart';
 
-void main() => runApp(const MyApp());
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:where_to/firebase_options.dart';
+
+//void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
